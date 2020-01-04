d33da019bbc594ea9106c837c3b53822657a743e
Jack Frolov <jackdev415@gmail.com>
2020-01-03 16:25:21 -0800

    set up global styles

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getStory } from '../services/hnAPI';
-import { StoryWrapper, StoryTitle, StoryMetaElement, StoryMeta } from '../styles/StoryStyles';
+import { StoryWrapper, StoryTitle, StoryMetaElement, StoryMeta, GlobalStyle } from '../styles/StoryStyles';
 
 export const Story = ({ storyID }) => {
 
@@ -12,18 +12,22 @@ export const Story = ({ storyID }) => {
 
 
     return story && story.url ? (
+        <React.Fragment>
+        <GlobalStyle />
         <StoryWrapper data-testid="story">
             <StoryTitle>
                 <a href={story.url}>{story.title}</a>
             </StoryTitle>
-
-            <span className="story__by" data-testid="story-by">
-                <StoryMetaElement color="#ccc">By:</StoryMetaElement> {story.by}
-            </span>
-            <br />
-            <span className="story__time" data-testid="story-time">
-                <StoryMetaElement color="#ccd">Time:</StoryMetaElement> {story.time}
-            </span>
+            <StoryMeta>
+                <span className="story__by" data-testid="story-by">
+                    <StoryMetaElement color="#ccc">By:</StoryMetaElement> {story.by}
+                </span>
+                <br />
+                <span className="story__time" data-testid="story-time">
+                    <StoryMetaElement color="#ccd">Posted:</StoryMetaElement> {story.time}
+                </span>
+            </StoryMeta>
         </StoryWrapper>
+        </React.Fragment>
     ) : null;
 }
--- src/styles/StoryStyles.js
+++ src/styles/StoryStyles.js
@@ -1,4 +1,11 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle  } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+`;
+
 
 export const StoryWrapper = styled.section`
     padding-top: 10px;
@@ -13,7 +20,7 @@ export const StoryWrapper = styled.section`
         margin-bottom: 0;
         padding-bottom: 0;
     }
-`
+`;
 
 export const StoryTitle = styled.h1`
     margin-botom: 5px;
@@ -27,13 +34,15 @@ export const StoryTitle = styled.h1`
         background-color: #f8dc3d;
         text-decoration: none;
     }
-`
+`;
 
-export const StoryMeta = styled.h1`
+export const StoryMeta = styled.div`
     font-style: italic;
-    line-height: 1.8;
-    margin: 0;
-    text-decoration: none;
+
+
+    > span:first-child {
+        margin-right: 10px;
+    }
 
     > span:not(:first-child):before {
         content: '-';
@@ -43,9 +52,9 @@ export const StoryMeta = styled.h1`
     .story__meta-bold {
         font-weight: bold;
     }
-`
+`;
 
 export const StoryMetaElement = styled.span`
     font-weight: bold;
     color: ${props => props.color || 'red'}
-`
+`;
